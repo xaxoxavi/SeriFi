ed932d992ba3a741b9ea96f5537a74588ad6ee98
rochismo <alexis.platino@gmail.com>
2019-03-21 20:01:13 +0100

    Refactored sendToArduino code

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const moment = require('moment');
 const verify = require('./verify-mock');
 const constants = require('./constants');
 
+let insertMode = false;
+
 // Response Object
 function Response(rfid, date, hour, weekDay, idMachine) {
     this.rfid = rfid;
@@ -41,39 +43,60 @@ parserNfc.on('data', async function (rfid) {
 
     moment.locale('es');
 
-    let date = moment().format("YYYY-DD-MM");
-    let hour = moment().format("HH:mm:ss");
-    let weekDay = moment().format("dddd");
+    const date = moment().format("YYYY-DD-MM");
+    const hour = moment().format("HH:mm:ss");
+    const weekDay = moment().format("dddd");
 
-    let responseObj = new Response(rfid, date, hour, weekDay, idMachine);
+    const responseObj = new Response(rfid, date, hour, weekDay, idMachine);
 
     console.log(responseObj);
 
-    let isVerified = await verify.verifyRFID(responseObj);
+    const verificationCode = await verify.verifyRFID(responseObj);
    
-    sendToArduino(isVerified,rfid);
+    sendToArduino(verificationCode,rfid);
     
     console.log(JSON.stringify(responseObj));
 
 });
 
-function sendToArduino(isVerified,rfid) {
+function handleCode(rfid, code, mode) {
+    // If it's insert mode we write what we have to write
+    portNfc.write(code);
+    portLcd.write(mode);
+    if (insertMode) {
+        io.emit('rfid', rfid);
+        
+    }
+}
 
+const modes = {
+    "0": handleCode,
+    "1": handleCode,
+    "2": (rfid) => {
+        insertMode = !insertMode;
+        const mode = insertMode ? rfid+"-Mode insercio" : "Fi Mode insercio";
+        handleCode('2', mode, rfid)
+    }
+}
+
+function sendToArduino(verificationCode,rfid) {
+    const mode = insertMode ? rfid + "-Mode insercio" : rfid;
+    modes[verificationCode](rfid, verificationCode, mode);
    
-    //Si esta en mode inserció enviam el codi rfid a la vista
-    if (insertMode && isVerified != 2) {
+    /** //Si esta en mode inserció enviam el codi rfid a la vista
+    if (insertMode && verificationCode != 2) {
         portNfc.write('1');
         portLcd.write(rfid+"-Mode insercio");
         io.emit('rfid', rfid);
 
     //Activam el mode insercio amb la targeta "admin"
-    } else if (!insertMode && isVerified == 2) {
+    } else if (!insertMode && verificationCode == 2) {
         insertMode = true;
         portNfc.write('2');
         portLcd.write(rfid+"-Mode insercio");
 
     //Desactivam mode insercio amb la targeta "admin"
-    } else if (insertMode && isVerified == 2) {
+    } else if (insertMode && verificationCode == 2) {
         insertMode = false;
         portNfc.write('2');
         portLcd.write("Fi Mode insercio");
@@ -81,13 +104,13 @@ function sendToArduino(isVerified,rfid) {
     //Si el mode insercio esta desactivat i reb una targeta normal
     //s'efectuara el fichatge normal
     } else {
-        if (isVerified == 1) {
+        if (verificationCode == 1) {
             portNfc.write('1');
         } else {
             portNfc.write('0');
         }
         portLcd.write(rfid);
 
-    }
+    } */
     console.log('[serialport] Sending info out of the serial port' .cyan);
 }
